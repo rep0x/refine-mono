7748dad604ac66768f06569fbf01140760525dab
Sven Reiser <svreiser@gmail.com>
2023-04-03 20:55:56 +0200

    eslintrc formatting and prettier tabWidth

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ['custom'],
   settings: {
     next: {
-      rootDir: ["apps/*/"],
+      rootDir: ['apps/*/'],
     },
   },
   rules: {
-    semi: ["error", "never"],
-    "sort-imports": [
-      "error",
+    semi: ['error', 'never'],
+    indent: ['error', 2],
+    'sort-imports': [
+      'error',
       {
         ignoreCase: false,
         ignoreDeclarationSort: false,
         ignoreMemberSort: false,
-        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
         allowSeparatedGroups: false,
       },
     ],
-    "sort-keys": [
-      "error",
-      "asc",
+    'sort-keys': [
+      'error',
+      'asc',
       { caseSensitive: true, natural: false, minKeys: 2 },
     ],
   },
-};
+}
